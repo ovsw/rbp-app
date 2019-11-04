4a52158ab4257dd9d80d121ad83fa473944197d3
Ovi Savescu <ovi@ovswebsites.com>
2019-11-04 16:11:08 +0200

    updated root packages to fix vulnerabilities, installed react icons

---
+++ web/src/elements/social-links.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import React from 'react' // eslint-disable-line
+import {jsx} from 'theme-ui'
+import {Link} from 'gatsby'
+
+const SocialLinks = () => {
+  return (
+    <>
+      <li><Link to='' /></li>
+    </>
+  )
+}
+
+export default SocialLinks
