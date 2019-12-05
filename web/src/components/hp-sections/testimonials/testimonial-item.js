9bc44a2e5b5afd6bad718f207be3de5b7595bd6e
Ovi Savescu <ovi@ovswebsites.com>
2019-12-05 18:38:46 +0200

    font size for testimonials

--- web/src/components/hp-sections/testimonials/testimonial-item.js
+++ web/src/components/hp-sections/testimonials/testimonial-item.js
@@ -13,7 +13,7 @@ const TestimonialItem = ({name = 'missing name', text = 'missing content'}) => {
       height: '100%',
       boxShadow: '0 0 12px -2px rgba(0,0,0,0.1)'
     }}>
-      <p sx={{mt: 0, fontWeight: 'light', fontStyle: 'italic', color: 'light', lineHeight: 'snug'}}>"{text}"</p>
+      <p sx={{mt: 0, fontWeight: 'light', fontStyle: 'italic', color: 'light', lineHeight: 'normal', fontSize: [1, 1, 1, 2]}}>"{text}"</p>
       <Styled.h5 as='h3' sx={{color: 'accent', fontWeight: 'light'}}>~{name}</Styled.h5>
     </div>
   )
