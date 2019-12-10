549170fef89e56ef449670e2272773193f63b400
Ovi Savescu <ovi@ovswebsites.com>
2019-12-10 17:43:06 +0200

    updated deskstructure to remove unused blog posts from desk

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -2,7 +2,7 @@ import S from '@sanity/desk-tool/structure-builder'
 import {MdSettings, MdPerson, MdFolder} from 'react-icons/md/'
 
 const hiddenDocTypes = listItem =>
-  !['category', 'author', 'post', 'siteSettings'].includes(listItem.getId())
+  !['category', 'author', 'post', 'page', 'siteSettings'].includes(listItem.getId())
 
 export default () =>
   S.list()
@@ -191,19 +191,19 @@ export default () =>
                 .title('Request Info')
                 .schemaType('page')
             ])),
-      S.listItem()
-        .title('Blog posts')
-        .schemaType('post')
-        .child(S.documentTypeList('post').title('Blog posts')),
-      S.listItem()
-        .title('Authors')
-        .icon(MdPerson)
-        .schemaType('author')
-        .child(S.documentTypeList('author').title('Authors')),
-      S.listItem()
-        .title('Categories')
-        .schemaType('category')
-        .child(S.documentTypeList('category').title('Categories')),
+      // S.listItem()
+      //   .title('Blog posts')
+      //   .schemaType('post')
+      //   .child(S.documentTypeList('post').title('Blog posts')),
+      // S.listItem()
+      //   .title('Authors')
+      //   .icon(MdPerson)
+      //   .schemaType('author')
+      //   .child(S.documentTypeList('author').title('Authors')),
+      // S.listItem()
+      //   .title('Categories')
+      //   .schemaType('category')
+      //   .child(S.documentTypeList('category').title('Categories')),
       // This returns an array of all the document types
       // defined in schema.js. We filter out those that we have
       // defined the structure above
