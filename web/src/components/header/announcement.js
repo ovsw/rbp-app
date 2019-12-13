2ca4f2034bc8834a77e707e1707ad5b9dd88ab1e
Ovi Savescu <ovi@ovswebsites.com>
2019-12-13 10:19:18 +0200

    announcement responsive improvements

--- web/src/components/header/announcement.js
+++ web/src/components/header/announcement.js
@@ -11,7 +11,7 @@ const Announcement = ({closeAlert}) => {
       <Container sx={{
         p: 0,
         pb: 1,
-        whiteSpace: 'nowrap',
+        whiteSpace: ['normal', 'normal', 'nowrap'],
         overflow: 'hidden',
         '@keyframes marquee': {
           '0%': {
@@ -28,9 +28,9 @@ const Announcement = ({closeAlert}) => {
           'animation-play-state': 'paused'
         }
       }}>
-        <p sx={{margin: 0, display: 'inline-block', pl: '100%', animation: 'marquee 10s linear infinite'}}>This is a text announcement. This text can be anything, and even contain <a href='https://www.ramblingpines.com/'>links</a></p>
+        <p sx={{margin: 0, fontSize: [1, 1, 2], display: 'inline-block', pl: ['0', '0', '100%'], pr: [5, 5, '0'], animation: ['none', 'none', 'marquee 10s linear infinite']}}>This is a text announcement. This text can be anything, and even contain <a href='https://www.ramblingpines.com/'>links</a></p>
       </Container>
-      <button onClick={() => closeAlert()} sx={{position: 'absolute', right: '0', bottom: '3px', zIndex: '10', bg: 'transparent', border: 'none', color: 'light', cursor: 'pointer'}}><FaTimes /><span sx={{position: 'relative', bottom: '3px'}}>close</span></button>
+      <button onClick={() => closeAlert()} sx={{position: 'absolute', px: '2', right: '0', top: '4px', zIndex: '10', bg: 'transparent', border: 'none', color: 'light', cursor: 'pointer'}}><FaTimes sx={{fontSize: '1rem'}} /><span sx={{display: ['none', 'none', 'inline'], position: 'relative', bottom: '3px'}}>close</span></button>
     </div>
   )
 }
